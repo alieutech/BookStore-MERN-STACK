6beadd06664dd21d4fb65c4b7556622439b7a6c0
Alieu Saidy <alieusaidy1999@gmail.com>
2024-11-24 09:51:50 +0000

    Bookstore frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import GetBook from './pages/GetBook';
+import Home from './pages/Home';
+import UpdateBook from './pages/UpdateBook';
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/books/create' element={<CreateBook />}/>
+      <Route path='/books/delete' element={<DeleteBook />}/>
+      <Route path='books/get/:id' element={<GetBook />}/>
+      <Route path='/books/update/:id' element={<UpdateBook />}/>  
+    </Routes>
   )
 }
 
-export default App
+export default App;
