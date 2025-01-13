99531e5888f4d90049d415febc62ba8366366302
Alieu Saidy <alieusaidy1999@gmail.com>
2025-01-13 16:14:57 +0000

    fix some erors in the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const mongoDB = require('./config/config')
-const cors = require('cors')
+const mongoDB = require('./config/config');
+const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
-const errorHandler = require('./middleware/errorHandler')
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3333
 
 // connect to mongoDB
-mongoDB()
+mongoDB();
 
 // CORS configuration
 const corsOptions = {
@@ -23,16 +24,16 @@ app.use(express.json())
 // build in middleware to handle urlencoded form data
 app.use(express.urlencoded({extended: false}));
 
-app.use('/books', require('./routers/books'))
+app.use('/books', require('./routers/books'));
 
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-}
+};
 
 mongoose.connection.once('open', () => {
     console.log('Connected to the mongoDB');
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
+  plugins: [react()],
+  server: {
     proxy: {
       "/books": {
         target: "http://localhost:3333", // Backend server 
-        changeOrigin: true, 
+        changeOrigin: true,
       },
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000KB
   },
 });
