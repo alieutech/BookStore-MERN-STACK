eb3ce3749ac92fb111166a677c89d2e0b6cdfbc8
Alieu Saidy <alieusaidy1999@gmail.com>
2025-01-20 16:57:24 +0000

    added a dockerfile configuration and docker-compose file

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,9 @@ app.use('/books', require('./routers/books'));
 app.use(errorHandler);
 
 if(process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 };
 
