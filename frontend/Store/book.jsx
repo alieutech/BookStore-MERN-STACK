da28356984afacda2267187ce860777df741b241
Alieu Saidy <alieusaidy1999@gmail.com>
2025-01-31 14:56:02 +0000

    updated

--- frontend/Store/book.jsx
+++ frontend/Store/book.jsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 // API Base URL 
-const API_BASE_URL = "http://localhost:3333";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3333";
 
 export const useBookStore = create((set) => ({
   books: [],
